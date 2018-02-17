6d945d6b18abb19713e6aea3fc78b8269e97836c
Kelin2025 <kelin2025@yandex.ru>
2018-02-17 03:28:12 +0300

    Looks like it's done

--- mixin.js
+++ mixin.js
@@ -1,12 +1,37 @@
-const stash = {}
+import EventsBus from 'nanoevents'
+
+const bus = new EventsBus()
+const components = []
+
+const pick = keys => obj =>
+  keys.reduce((res, key) => ({ ...res, [key]: obj[key] }), {})
+
+const save = comp => {
+  localStorage.setItem(
+    `stash-${comp.$options.name}`,
+    JSON.stringify(pick(Object.keys(comp.$options.stash))(comp))
+  )
+}
 
 export default {
   data() {
     if (!this.$options.stash) return {}
-    if (!(this.$vnode.tag in stash)) {
-      stash[this.$vnode.tag] = this.$options.stash
-    }
-    return stash[this.$vnode.tag]
+    components.push(this)
+    const ls = localStorage.getItem(`stash-${this.$options.name}`)
+    const res = ls && ls !== '{}' ? JSON.parse(ls) : this.$options.stash
+    return { ...res }
   },
-  beforeDestroy() {}
+  created() {
+    this._stashWatchers = Object.keys(this.$options.stash).map(k =>
+      this.$watch(k, val => bus.emit(`stash:${this.$options.name}`, k, val))
+    )
+    this._stashListener = bus.on(`stash:${this.$options.name}`, (k, val) => {
+      this[k] = val
+    })
+  },
+  beforeDestroy() {
+    this._stashWatchers.forEach(unwatch => unwatch())
+    this._stashListener()
+    save(this)
+  }
 }
